aba95d947b21d42414a83735e05c5c93f613ac4c
Olekciiy Anurov-Prihodko <anyrov.prihodko@gmail.com>
2021-10-05 15:35:14 +0300

    1

--- src/pages/NotFoundPage.js
+++ src/pages/NotFoundPage.js
@@ -2,7 +2,7 @@ export default function NotFoundPage() {
   return (
     <h2>
       Error 404 <br />
-      Sorry, but there is no such page on the site :(
+      Sorry, but there is no such page on the site.
     </h2>
   );
 }
